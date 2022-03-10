aed72669b88e40728ea6e018cab60da334c53b5d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-09 20:03:40 -0500

    :bug: Fix Test

--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "./test_deps.ts";
-import clear from "./mod.ts";
+import { clear } from "./mod.ts";
 
 Deno.test("Test It returns nothing", () => {
   assertEquals(clear(), undefined);
